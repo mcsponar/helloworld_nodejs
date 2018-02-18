8872201cbfdfecbb39aca21fd048eee951dc7737
Matthew Sponar <mcsponar@gmail.com>
2018-02-18 15:19:20 -0500

    Add an HTTP Header

--- app.js
+++ app.js
@@ -4,14 +4,13 @@
  * and open the template in the editor.
  */
 const http = require('http');
-var dt = require('./myfirstmodule');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("The date and time are currently: " + dt.myDateTime());
+    res.write('Hello World!');
     res.end();
 }).listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
