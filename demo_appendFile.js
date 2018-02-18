d2bc6426885c12c1201d1b53c36332cb2f2a464c
Matthew Sponar <mcsponar@gmail.com>
2018-02-18 15:54:59 -0500

    Create Files Appending

---
+++ demo_appendFile.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+    fs.appendFile('mynewfile1.txt', 'Hello content! ' + Date() + '\n',
+            function (err) {
+                if (err)
+                    throw err;
+                console.log('Saved!');
+            });
+}).listen(8080);
