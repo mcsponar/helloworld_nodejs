b20cc3b01e39712cec5b0b19dd6effe990553e94
Matthew Sponar <mcsponar@gmail.com>
2018-02-18 16:39:08 -0500

    Using a package from NPM

--- app.js
+++ app.js
@@ -3,14 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-const http = require('http');
+var http = require('http');
+var uc = require('upper-case');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = 'localhost';
+const port = 8080;
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(req.url);
+    res.write(uc("Hello World!"));
     res.end();
 }).listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
