d972f08f6e8ed4a22fa4e6524ee2e4ce817ed425
Matthew Sponar <mcsponar@gmail.com>
2018-02-25 10:26:20 -0500

    create table
    https://www.w3schools.com/nodejs/nodejs_mysql_create_table.asp

---
+++ db.js
@@ -0,0 +1,28 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function Mother(){
+    this.host = "localhost";
+    this.db_user = "demo";
+    this.db_pw = "pa$$word";
+    this.db = "mydb";
+}
+
+//mother.prototype.host = function (){
+//  return "localhost";  
+//};
+
+//mother.prototype.db_user = function () {
+//    return "demo";
+//};
+
+//mother.prototype.db_pw = function () {
+//    return "pa$$word";
+//};
+
+//mother.prototype.db = function (){
+//    return "mydb";
+//};
---
+++ demo_create_table.js
@@ -0,0 +1,26 @@
+var mysql = require('mysql');
+var db = require('./db');
+
+var host = "localhost";
+var db_user = "demo";
+var db_pw = "pa$$word";
+var db = "mydb";
+
+var con = mysql.createConnection({
+    host: host,
+    user: db_user,
+    password: db_pw,
+    database: db
+});
+
+con.connect(function(err){
+    if(err)throw err;
+    console.log("Connected!");
+    var sql = "CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY,\n\
+name VARCHAR(255), address VARCHAR(255))";
+    
+    con.query(sql, function(err, result){
+        if(err) throw err;
+        console.log("Table Created");
+    });
+});
