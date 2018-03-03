9b846bbf7b11cf3c73a2f63e920dd7280e8e2df6
Matthew Sponar <mcsponar@gmail.com>
2018-03-03 12:59:02 -0500

    drop table

--- demo_create_table.js
+++ demo_create_table.js
@@ -21,6 +21,6 @@ name VARCHAR(255), address VARCHAR(255))";
     
     con.query(sql, function(err, result){
         if(err) throw err;
-        console.log("Table Created");
+        console.log("Table Created\n" + result);
     });
 });
---
+++ demo_db_drops_if_exists.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "demo",
+    password: "pa$$word",
+    database: "mydb"
+});
+
+con.connect(function(err){
+   if (err) throw err;
+   var sql = "DROP TABLE IF EXISTS customers";
+   con.query(sql, function(err, result){
+       if(err) throw err;
+       console.log(result);
+   });
+});
